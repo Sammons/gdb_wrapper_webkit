284933232e16ca5ca37db59056fa2f3c903b14ea
Sammons <bsammnz@gmail.com>
2015-01-30 22:01:44 -0800

    removing stuff from main

---
+++ main.js
@@ -0,0 +1,11 @@
+global.$ = $;
+
+var abar = require('address_bar');
+var folder_view = require('folder_view');
+var path = require('path');
+var shell = require('nw.gui').Shell;
+
+$(document).ready(function() {
+
+  
+});
